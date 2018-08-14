96e897e7810d79eb2dc4104e2e887576a00bafd0
Bernard Tison <bernard.tison@gmail.com>
2018-08-14 15:05:15 +0200

    jms configuration

--- src/main/java/com/acme/ride/dispatch/DispatchServiceApplication.java
+++ src/main/java/com/acme/ride/dispatch/DispatchServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootConfiguration
 @ComponentScan
 @EnableAutoConfiguration
+@EnableJms
 public class DispatchServiceApplication {
 
     public static void main(String[] args) {
