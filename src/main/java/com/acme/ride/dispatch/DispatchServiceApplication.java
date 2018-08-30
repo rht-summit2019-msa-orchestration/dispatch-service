bb95a1a1810d2ab0c10b8d6af331013d90b012ec
Bernard Tison <bernard.tison@gmail.com>
2018-08-30 10:46:56 +0200

    replaced spring-boot-starter-jta-narayana with narayana-spring-boot-starter from SnowDrop project

--- src/main/java/com/acme/ride/dispatch/DispatchServiceApplication.java
+++ src/main/java/com/acme/ride/dispatch/DispatchServiceApplication.java
@@ -3,16 +3,13 @@ package com.acme.ride.dispatch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.jta.narayana.DbcpXADataSourceWrapper;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootConfiguration
 @ComponentScan
 @EnableAutoConfiguration
 @EnableJms
-@Import(DbcpXADataSourceWrapper.class)
 public class DispatchServiceApplication {
 
     public static void main(String[] args) {
