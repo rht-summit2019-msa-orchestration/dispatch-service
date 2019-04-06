3b3b264e2699b9e83871661e8aa64e4d5c78cb5e
Bernard Tison <bernard.tison@gmail.com>
2019-04-06 10:15:58 +0200

    refactoring startup sequence of the application to make sure that the process is fully deployed by system startup

--- src/main/java/com/acme/ride/dispatch/DispatchServiceApplication.java
+++ src/main/java/com/acme/ride/dispatch/DispatchServiceApplication.java
@@ -1,19 +1,8 @@
 package com.acme.ride.dispatch;
 
-import java.util.Collection;
-
-import org.jbpm.kie.services.impl.CustomIdKModuleDeploymentUnit;
-import org.jbpm.services.api.DeploymentService;
-import org.jbpm.services.api.RuntimeDataService;
-import org.jbpm.services.api.model.ProcessDefinition;
-import org.kie.api.KieServices;
-import org.kie.api.runtime.KieContainer;
-import org.kie.api.runtime.query.QueryContext;
-import org.kie.internal.runtime.conf.RuntimeStrategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
@@ -42,36 +31,14 @@ public class DispatchServiceApplication {
     }
 
     @Bean
-    CommandLineRunner deployAndValidate() {
+    CommandLineRunner startKafkaListeners() {
         return new CommandLineRunner() {
-
-            @Value("${dispatch.deployment.id}")
-            private String deploymentId;
-
-            @Autowired
-            private DeploymentService deploymentService;
-
-            @Autowired
-            private RuntimeDataService runtimeDataService;
 
             @Autowired
             private KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry;
 
             @Override
             public void run(String... strings) throws Exception {
-                CustomIdKModuleDeploymentUnit unit = new CustomIdKModuleDeploymentUnit(deploymentId, "com.acme.ride.dispatch", "dispatch-service", "1.0.0");
-
-                unit.setStrategy(RuntimeStrategy.PER_REQUEST);
-
-                KieContainer kieContainer = KieServices.Factory.get().newKieClasspathContainer();
-                unit.setKieContainer(kieContainer);
-                log.info("Service up and running");
-
-                deploymentService.deploy(unit);
-
-                Collection<ProcessDefinition> processes = runtimeDataService.getProcesses(new QueryContext());
-                processes.forEach(p -> log.info(p.getName()));
-
                 kafkaListenerEndpointRegistry.start();
             }
         };
